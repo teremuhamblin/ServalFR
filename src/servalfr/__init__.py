d02ec154bc833ccfb98769d7893c388c0e55c36e
98731 <105843577+teremuhamblin@users.noreply.github.com>
2026-09-11 08:03:03 +0200

    Create __init__.py

---
+++ src/servalfr/__init__.py
@@ -0,0 +1,9 @@
+"""
+ServalFR - Simulation logistique générique.
+Organisation, scénarios, unités.
+Aucun calcul d’armement.
+"""
+
+from .main import run_simulation, main
+
+__all__ = ["run_simulation", "main"]
